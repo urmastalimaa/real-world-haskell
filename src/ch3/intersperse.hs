862c61b68faa8da9966dae5df7e96b9bd97d833a
urmastalimaa <urmas7@gmail.com>
2015-12-12 21:33:01 +0200

    Add chatper3: intersperse

---
+++ src/ch3/intersperse.hs
@@ -0,0 +1,6 @@
+module Intersperse where
+
+intersperse:: a -> [[a]] -> [a]
+intersperse _ [] = []
+intersperse _ [x] = x
+intersperse separator (x:xs) = (x ++ [separator]) ++ intersperse separator xs
