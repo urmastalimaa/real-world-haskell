e6cfd6035ef72b36ab20e4c791db7b70a37700aa
urmastalimaa <urmas7@gmail.com>
2015-12-12 22:45:40 +0200

    Add chapter3: calculateDirection

--- src/ch3/Direction.hs
+++ src/ch3/Direction.hs
@@ -1,3 +1,20 @@
-module Direction(Direction(Left, Right, Straight)) where
+module Direction(
+  Direction(Left, Right, Straight),
+  calculateDirection
+) where
+import Point(Point(Point))
 
 data Direction = Left | Right | Straight
+
+calculateDirection:: Point -> Point -> Point -> Direction
+calculateDirection a b c = angleToDirection (ccw a b c)
+
+  where ccw (Point ax ay) (Point bx by) (Point cx cy) =
+          (bx - ax) * (cy - ay) - (by - ay) * (cx - ax)
+
+        angleToDirection angle
+          | angle > 0 = Direction.Left
+          | angle < 0 = Direction.Right
+          | otherwise = Direction.Straight
+
+
