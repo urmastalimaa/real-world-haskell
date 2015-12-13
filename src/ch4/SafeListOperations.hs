363f732d893c0845a37dc21b858fb66448acdf0c
urmastalimaa <urmas7@gmail.com>
2015-12-13 14:14:57 +0200

    Add chapter 4: safe list operations

---
+++ src/ch4/SafeListOperations.hs
@@ -0,0 +1,17 @@
+module CH4.SafeListOperations(safeHead, safeTail, safeLast, safeInit) where
+
+safeHead:: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead xs = Just . head $ xs
+
+safeTail:: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail xs = Just . tail $ xs
+
+safeLast:: [a] -> Maybe a
+safeLast [] = Nothing
+safeLast xs = Just . last $ xs
+
+safeInit:: [a] -> Maybe [a]
+safeInit [] = Nothing
+safeInit xs = Just . init $ xs
