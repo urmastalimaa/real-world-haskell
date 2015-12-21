bea8995cd01a5269d6381954222081963cba9a6e
urmastalimaa <urmas7@gmail.com>
2015-12-21 23:17:20 +0200

    Add takeWhile

--- src/ch4/folds.hs
+++ src/ch4/folds.hs
@@ -9,3 +9,13 @@ asInt xs = foldl (\acc c -> acc * 10 + digitToInt c) 0 xs
 
 concat :: [[a]] -> [a]
 concat = foldr (++) []
+
+-- recursion
+takeWhile1 :: (a -> Bool) -> [a] -> [a]
+takeWhile1 pred (x:xs)
+  | pred x    = x : takeWhile1 pred xs
+  | otherwise = []
+takeWhile1 _ [] = []
+
+takeWhile2 :: (a -> Bool) -> [a] -> [a]
+takeWhile2 pred = foldr (\x acc -> if pred x then x:acc else acc) []
